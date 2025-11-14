77c2349f9bed9f00e18d7d2c53c377d5b8706f79
CameraSkunk <daniel.d.coelho@gmail.com>
2025-11-14 17:14:23 -0500

    This now accepts a path as an argument

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -22,7 +24,11 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("============= END ===============")
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
