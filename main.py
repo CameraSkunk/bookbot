48514fc4af5e795dd931bbb5b28c1f40533937b7
CameraSkunk <daniel.d.coelho@gmail.com>
2025-11-14 13:34:36 -0500

    report created

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from stats import word_count, get_chars_dict
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        return file_contents
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    text = get_book_text(path_to_file)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-
-    print(f"Found {word_count(text)} total words")
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 main()
